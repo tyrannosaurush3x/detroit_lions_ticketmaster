9467c4715debac5b166faeff2dd11a776d332056
magnamarketing <96262983+magnamarketing@users.noreply.github.com>
2022-04-12 11:44:30 -0700

    Update artist-detail-page.js
    https://www.ticketmaster.com/detroit-lions-tickets/artist/805936

--- artist-detail-page.js
+++ artist-detail-page.js
@@ -1,49 +1,49 @@
 Evergage.initSitemap({
-        global: {},
-        pageTypeDefault: {
-            name: "TicketmasterDefault",
-        },
-        pageTypes: [{
-            name: "tmArtistPage",
-            action: "Ticketmaster Artist",
-            isMatch: () => true,
-        }]
-    });
+    global: {},
+    pageTypeDefault: {
+        name: "TicketmasterDefault",
+    },
+    pageTypes: [{
+        name: "tmArtistPage",
+        action: "Ticketmaster Artist",
+        isMatch: () => true,
+    }]
+});
 
-    // User ID merge event 
+// User ID merge event 
 
-    const sendUserId = () => { 
-        if (/persistId/.test(window.location.href)) {
-            try {
-                const anonId = location.href.split("persistId=")[1];
-                Evergage.sendEvent({
-                    name: "Ticketmaster ID merge",
-                    action: "Ticketmaster ID merge",
-                    user: {
-                        attributes: {
-                            persistId: persistId
-                        }
-                    }
-                })
-            } catch (e) {
-                Evergage.sendEvent({
-                    name: "Ticketmaster ID merge Failed",
-                    action: "Ticketmaster ID merge Failed"
-                })
-            }
-        }
-        
-    }
-
-    // Abandoned Browse Event 
-
-    const abandonedBrowse = () => {
-        Evergage.DisplayUtils.pageExit(1000).then(() => {
+const sendUserId = () => {
+    if (/persistId/.test(window.location.href)) {
+        try {
+            const anonId = location.href.split("persistId=")[1];
             Evergage.sendEvent({
-                action: "Ticketmaster Abandoned Browse"
+                name: "Ticketmaster ID merge",
+                action: "Ticketmaster ID merge",
+                user: {
+                    attributes: {
+                        persistId: persistId
+                    }
+                }
             })
-        })
+        } catch (e) {
+            Evergage.sendEvent({
+                name: "Ticketmaster ID merge Failed",
+                action: "Ticketmaster ID merge Failed"
+            })
+        }
     }
-    
-    sendUserId();
-    abandonedBrowse();
+
+}
+
+// Abandoned Browse Event 
+
+const abandonedBrowse = () => {
+    Evergage.DisplayUtils.pageExit(1000).then(() => {
+        Evergage.sendEvent({
+            action: "Ticketmaster Abandoned Browse"
+        })
+    })
+}
+
+sendUserId();
+abandonedBrowse();
