f0d1bb93ccee5c6f4deb5ecfc4d3d0245417cf1f
magnamarketing <96262983+magnamarketing@users.noreply.github.com>
2022-04-12 11:41:45 -0700

    Create artist-detail-page-branded.js.js

---
+++ artist-detail-page-branded.js.js
@@ -0,0 +1,49 @@
+Evergage.initSitemap({
+    global: {},
+    pageTypeDefault: {
+        name: "TicketmasterDefault",
+    },
+    pageTypes: [{
+        name: "tmArtistPageBranded",
+        action: "Ticketmaster Artist Branded",
+        isMatch: () => true,
+    }]
+});
+
+// User ID merge event 
+
+const sendUserId = () => {
+    if (/persistId/.test(window.location.href)) {
+        try {
+            const anonId = location.href.split("persistId=")[1];
+            Evergage.sendEvent({
+                name: "Ticketmaster ID merge",
+                action: "Ticketmaster ID merge",
+                user: {
+                    attributes: {
+                        persistId: persistId
+                    }
+                }
+            })
+        } catch (e) {
+            Evergage.sendEvent({
+                name: "Ticketmaster ID merge Failed",
+                action: "Ticketmaster ID merge Failed"
+            })
+        }
+    }
+
+}
+
+// Abandoned Browse Event 
+
+const abandonedBrowse = () => {
+    Evergage.DisplayUtils.pageExit(1000).then(() => {
+        Evergage.sendEvent({
+            action: "Ticketmaster Abandoned Browse"
+        })
+    })
+}
+
+sendUserId();
+abandonedBrowse();
