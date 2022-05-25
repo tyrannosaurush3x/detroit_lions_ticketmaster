d950deb6cafc65d77eb8335d0f8f6a11bcc5afb7
magnamarketing <96262983+magnamarketing@users.noreply.github.com>
2022-05-25 10:17:45 -0700

    Update artist-detail-page.js

--- artist-detail-page.js
+++ artist-detail-page.js
@@ -1,52 +1,45 @@
-Evergage.init({
+let timerVar = setInterval(() => {
+    if (typeof Evergage != 'undefined') {
+        
+        Evergage.init({
    
-}).then(() => {
-      
-    Evergage.initSitemap({
-        global: {},
-        pageTypeDefault: {
-            name: "TicketmasterDefault",
-        },
-        pageTypes: [{
-            name: "tmArtistPage",
-            action: "Ticketmaster Artist",
-            isMatch: () => true,
-        }]
-    });   // Initializes the Sitemap
-});
+        }).then(() => {
+              
+            Evergage.initSitemap({
+                global: {},
+                pageTypeDefault: {
+                    name: "TicketmasterDefault",
+                },
+                pageTypes: [{
+                    name: "tmArtistPage",
+                    action: "Ticketmaster Artist",
+                    isMatch: () => true,
+                }]
+            });   // Initializes the Sitemap
+        });
 
-const sendUserId = () => {
-    if (/persistId/.test(window.location.href)) {
-        try {
-            const anonId = location.href.split("persistId=")[1];
-            Evergage.sendEvent({
-                name: "Ticketmaster ID merge",
-                action: "Ticketmaster ID merge",
-                user: {
-                    attributes: {
-                        persistId: persistId
-                    }
+        const sendUserId = () => {
+            if (/persistUserId/.test(window.location.href)) {
+                try {
+                    const anonId = location.href.split("persistUserId=")[1];
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge",
+                        action: "Ticketmaster ID merge",
+                        user: {
+                            attributes: {
+                                persistId: anonId
+                            }
+                        }
+                    })
+                } catch (e) {
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge Failed",
+                        action: "Ticketmaster ID merge Failed"
+                    })
                 }
-            })
-        } catch (e) {
-            Evergage.sendEvent({
-                name: "Ticketmaster ID merge Failed",
-                action: "Ticketmaster ID merge Failed"
-            })
+            }
+        
         }
+        sendUserId();
     }
-
-}
-
-// Abandoned Browse Event 
-
-const abandonedBrowse = () => {
-    Evergage.DisplayUtils.pageExit(1000).then(() => {
-        Evergage.sendEvent({
-            action: "Ticketmaster Abandoned Browse"
-        })
-    })
-}
-
-sendUserId();
-abandonedBrowse();
+}, 2000)
