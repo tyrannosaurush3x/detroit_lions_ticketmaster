20d730ae2aea21569d705204e779618c24a46d66
magnamarketing <96262983+magnamarketing@users.noreply.github.com>
2022-06-02 10:52:55 -0700

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,82 +1,63 @@
-Evergage.initSitemap({
-    global: {},
-    pageTypeDefault: {
-        name: "TicketmasterDefault ",
-    },
-    pageTypes: [{
-        name: "tmEventPage ",
-        action: "Ticketmaster Checkout Page",
-        isMatch: () => true,
-        listeners: [
-            Evergage.listener("click", "button[name='sign-in']", () => {
-                const emailAddress = $("input[name='email']").value;
-                Evergage.sendEvent({
-                    name: "Ticketmaster Sign-in",
-                    action: "Ticketmaster Sign-in",
-                    user: {
-                        attributes: {
-                            emailAddress: emailAddress
+let timerVar = setInterval(() => {
+    if (typeof Evergage != 'undefined') {
+        clearInterval(timerVar);
+        Evergage.init({
+
+        }).then(() => {
+            Evergage.initSitemap({
+                global: {},
+                pageTypeDefault: {
+                    name: "TicketmasterDefault ",
+                },
+                pageTypes: [{
+                    name: "tmEventPage ",
+                    action: "Ticketmaster Checkout Page",
+                    isMatch: () => true,
+                }]
+            });
+        })
+
+        const sendUserId = () => {
+            if (/persistId/.test(window.location.href)) {
+                try {
+                    const anonId = location.href.split("persistId=")[1];
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge",
+                        action: "Ticketmaster ID merge",
+                        user: {
+                            attributes: {
+                                persistId: persistId
+                            }
                         }
-                    }
-                })
-            }), 
-            Evergage.listener("submit", "button[type='submit']", () => {
-                //TODO Sign up click 
-            })
-        ]
-    }]
-});
-
-const sendUserId = () => {
-    if (/persistId/.test(window.location.href)) {
-        try {
-            const anonId = location.href.split("persistId=")[1];
-            Evergage.sendEvent({
-                name: "Ticketmaster ID merge",
-                action: "Ticketmaster ID merge",
-                user: {
-                    attributes: {
-                        persistId: persistId
-                    }
-                }
-            })
-        } catch (e) {
-            Evergage.sendEvent({
-                name: "Ticketmaster ID merge Failed",
-                action: "Ticketmaster ID merge Failed"
-            })
-        }
-    }
-}
-
-// Add to cart Event
-
-const sendAddToCart = () => {
-    Evergage.sendEvent({
-        itemAction: Evergage.ItemAction.AddToCart,
-        action: "Add To Cart",
-        cart: {
-            singleLine: {
-                Product: {
-                    _id: '$event_id$',
-                    price: '$event_name$', 
-                    quantity: '$ticket_quantity$'
+                    })
+                } catch (e) {
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge Failed",
+                        action: "Ticketmaster ID merge Failed"
+                    })
                 }
             }
         }
-    })
-}
 
-// Abandoned Browse Event 
+        // Add to cart Event
 
-const abandonedBrowse = () => {
-    Evergage.DisplayUtils.pageExit(1000).then(() => {
-        Evergage.sendEvent({
-            action: "Ticketmaster Abandoned Cart"
-        })
-    })
-}
+        const sendAddToCart = () => {
+            Evergage.sendEvent({
+                itemAction: Evergage.ItemAction.AddToCart,
+                action: "Add To Cart",
+                cart: {
+                    singleLine: {
+                        Product: {
+                            _id: '$event_id$',
+                            price: '$event_name$',
+                            quantity: '$ticket_quantity$'
+                        }
+                    }
+                }
+            })
+        }
 
-sendAddToCart();
-sendUserId();
-abandonedBrowse();
+        sendAddToCart();
+        sendUserId();
+    }
+}, 2000)
