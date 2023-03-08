18b135fe1a5d225ea0ddce82b8881c4f5686e3cd
tyrannosaurush3x <94508646+tyrannosaurush3x@users.noreply.github.com>
2023-03-08 13:52:28 -0500

    Create tm-account-manager-landing-pages.js

---
+++ tm-account-manager-landing-pages.js
@@ -0,0 +1,45 @@
+let timerVar = setInterval(() => {
+    if (typeof Evergage != 'undefined') {
+        
+        Evergage.init({
+   
+        }).then(() => {
+              
+            Evergage.initSitemap({
+                global: {},
+                pageTypeDefault: {
+                    name: "TicketmasterDefault",
+                },
+                pageTypes: [{
+                    name: "TM Account Manager Landing Pages",
+                    action: "TM Account Manager Landing Pages",
+                    isMatch: () => true,
+                }]
+            });   // Initializes the Sitemap
+        });
+
+        const sendUserId = () => {
+            if (/persistUserId/.test(window.location.href)) {
+                try {
+                    const anonId = location.href.split("persistUserId=")[1].split('&')[0];
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge",
+                        action: "Ticketmaster ID merge",
+                        user: {
+                            attributes: {
+                                persistId: anonId
+                            }
+                        }
+                    })
+                } catch (e) {
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge Failed",
+                        action: "Ticketmaster ID merge Failed"
+                    })
+                }
+            }
+        
+        }
+        sendUserId();
+    }
+}, 2000)
