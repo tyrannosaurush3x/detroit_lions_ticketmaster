769d70a534bd7610b227727ba17841fdf99dfe2e
tyrannosaurush3x <94508646+tyrannosaurush3x@users.noreply.github.com>
2023-03-08 14:06:56 -0500

    Create tm-account-manager-order-confirmation.js

---
+++ tm-account-manager-order-confirmation.js
@@ -0,0 +1,55 @@
+let timerVar = setInterval(() => {
+    if (typeof Evergage != 'undefined') {
+        clearInterval(timerVar);
+        Evergage.init({
+
+        }).then(() => {
+            Evergage.initSitemap({
+                global: {},
+                pageTypeDefault: {
+                    name: 'TicketmasterDefault',
+                },
+                pageTypes: [{
+                    name: 'TM Account Manager Order Confirmation',
+                    action: 'TM Account Manager Order Confirmation',
+                    isMatch: () => true,
+                    itemAction: Evergage.ItemAction.Purchase,
+                    order: {
+                        Product: {
+                            orderId: '$order_id$',
+                            totalValue: '$face_value$',
+                            lineItems: {
+                                _id: ['$event_id$'],
+                                price: [($face_value$ / $ticket_quantity$).toFixed(2)],
+                                quantity: [$ticket_quantity$]
+                            }
+                        }
+                    }
+                }],
+            });
+        });
+        const sendUserId = () => {
+            if (/persistUserId/.test(window.location.href)) {
+                try {
+                    const anonId = location.href.split("persistUserId=")[1].split('&')[0];
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge",
+                        action: "Ticketmaster ID merge",
+                        user: {
+                            attributes: {
+                                persistId: anonId
+                            }
+                        }
+                    })
+                } catch (e) {
+                    Evergage.sendEvent({
+                        name: "Ticketmaster ID merge Failed",
+                        action: "Ticketmaster ID merge Failed"
+                    })
+                }
+            }
+
+        }
+        sendUserId();
+    }
+}, 2000)
